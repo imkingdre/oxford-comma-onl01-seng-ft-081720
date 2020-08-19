2e2eab688cd5d95d50d64d47af1362122b5a5cc9
Andre King <andreking1031@gmail.com>
2020-08-19 16:58:46 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
     if array.size == 2 
       array.insert(1, "and").join(" ")
       elsif array.size > 2
-      array[-1].insert(0, "and")
+      array[-1].insert(0, "and ")
       array.join(", ")
     end
 
